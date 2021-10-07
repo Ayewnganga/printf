4153882bc7efae0c26afe64ebabacff5713b35c4
Ayub <ayewnganga@gmail.com>
2021-10-07 22:42:23 +0300

    header

--- main.h
+++ main.h
@@ -5,6 +5,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
 typedef struct Args
 {
 	char symbol;
